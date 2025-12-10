c605ef26014b28d856af6690c24d0f11afea81c5
Boshen <boshenc@gmail.com>
2025-12-10 12:03:46 +0800

    feat: implement path resolution and property unset  (#1)

--- tests/main.rs
+++ tests/main.rs
@@ -1,4 +1,9 @@
-use editorconfig_parser::{Charset, EditorConfig, EndOfLine, IdentStyle, MaxLineLength};
+use std::path::Path;
+
+use editorconfig_parser::{
+    Charset, EditorConfig, EditorConfigProperties, EditorConfigProperty::Value, EndOfLine,
+    IndentStyle, MaxLineLength,
+};
 
 #[test]
 fn empty() {
@@ -51,12 +56,12 @@ fn values() {
     );
     assert_eq!(editor_config.sections().len(), 1);
     let properties = &editor_config.sections()[0].properties;
-    assert_eq!(properties.charset, Some(Charset::Utf8));
-    assert_eq!(properties.insert_final_newline, Some(true));
-    assert_eq!(properties.end_of_line, Some(EndOfLine::Lf));
-    assert_eq!(properties.indent_style, Some(IdentStyle::Space));
-    assert_eq!(properties.indent_size, Some(2));
-    assert_eq!(properties.max_line_length, Some(MaxLineLength::Number(80)));
+    assert_eq!(properties.charset, Value(Charset::Utf8));
+    assert_eq!(properties.insert_final_newline, Value(true));
+    assert_eq!(properties.end_of_line, Value(EndOfLine::Lf));
+    assert_eq!(properties.indent_style, Value(IndentStyle::Space));
+    assert_eq!(properties.indent_size, Value(2));
+    assert_eq!(properties.max_line_length, Value(MaxLineLength::Number(80)));
 }
 
 #[test]
@@ -68,11 +73,110 @@ fn max_line_length_off() {
     );
     assert_eq!(editor_config.sections().len(), 1);
     let properties = &editor_config.sections()[0].properties;
-    assert_eq!(properties.max_line_length, Some(MaxLineLength::Off));
+    assert_eq!(properties.max_line_length, Value(MaxLineLength::Off));
 }
 
 #[test]
-fn parse() {}
+fn resolve() {
+    let editor_config = EditorConfig::parse(
+        "
+        # *
+        [*]
+        charset = utf-8
+        insert_final_newline = true
+        end_of_line = lf
+        indent_style = space
+        indent_size = 2
+        max_line_length = 80
+
+        ; foo
+        [*.foo]
+        charset = latin1
+        insert_final_newline = false
+        end_of_line = crlf
+        indent_style = tab
+        indent_size = 4
+        max_line_length = 100
+
+        [*.{ts,tsx,js,jsx,mts,cts}]
+        indent_size = 8
+        max_line_length = 120
+
+        [*.rs]
+        max_line_length = 140
+
+        [**/__snapshots__/**]
+        max_line_length = 160
+    ",
+    );
+
+    let path = Path::new("/");
+    let all = editor_config.resolve(path);
+    assert_eq!(all.charset, Value(Charset::Utf8));
+    assert_eq!(all.insert_final_newline, Value(true));
+    assert_eq!(all.end_of_line, Value(EndOfLine::Lf));
+    assert_eq!(all.indent_style, Value(IndentStyle::Space));
+    assert_eq!(all.indent_size, Value(2));
+    assert_eq!(all.max_line_length, Value(MaxLineLength::Number(80)));
+
+    let properties = editor_config.resolve(&path.join("file.foo"));
+    assert_eq!(properties.charset, Value(Charset::Latin1));
+    assert_eq!(properties.insert_final_newline, Value(false));
+    assert_eq!(properties.end_of_line, Value(EndOfLine::Crlf));
+    assert_eq!(properties.indent_style, Value(IndentStyle::Tab));
+    assert_eq!(properties.indent_size, Value(4));
+    assert_eq!(properties.max_line_length, Value(MaxLineLength::Number(100)));
+
+    for ext in ["ts", "tsx", "js", "jsx", "mts", "cts"] {
+        assert_eq!(
+            editor_config.resolve(&path.join("file").with_extension(ext)),
+            EditorConfigProperties {
+                indent_size: Value(8),
+                max_line_length: Value(MaxLineLength::Number(120)),
+                ..all.clone()
+            }
+        );
+    }
+
+    assert_eq!(
+        editor_config.resolve(&path.join("file.rs")),
+        EditorConfigProperties {
+            max_line_length: Value(MaxLineLength::Number(140)),
+            ..all.clone()
+        }
+    );
+
+    assert_eq!(
+        editor_config.resolve(&path.join("dir").join("__snapshots__").join("file")),
+        EditorConfigProperties {
+            max_line_length: Value(MaxLineLength::Number(160)),
+            ..all.clone()
+        }
+    );
+}
 
 #[test]
-fn unset() {}
+fn unset() {
+    let editor_config = EditorConfig::parse(
+        "
+        [*]
+        charset = utf-8
+        insert_final_newline = true
+        end_of_line = lf
+        indent_style = space
+        indent_size = 2
+        max_line_length = 80
+
+        [*.foo]
+        charset = unset
+        insert_final_newline = unset
+        end_of_line = unset
+        indent_style = unset
+        indent_size = unset
+        max_line_length = unset
+    ",
+    );
+    let path = Path::new("/").join("file.foo");
+    let properties = editor_config.resolve(&path);
+    assert_eq!(properties, EditorConfigProperties::default());
+}
