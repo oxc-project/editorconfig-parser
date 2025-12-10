e5b72b2774f0db7b905962a7a613ca9848b7794f
Boshen <boshenc@gmail.com>
2025-12-10 11:50:26 +0800

    feat: implement path resolution and property `unset`

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use std::path::Path;
 
+use globset::Glob;
+
 #[derive(Debug, Default, Clone)]
 pub struct EditorConfig {
     /// Set to true to tell the core not to check any higher directory for EditorConfig settings for on the current filename.
@@ -28,42 +30,50 @@ pub struct EditorConfigSection {
 }
 
 #[derive(Debug, Default, Clone, Eq, PartialEq)]
+pub enum EditorConfigProperty<T> {
+    #[default]
+    None,
+    Unset,
+    Value(T),
+}
+
+#[derive(Debug, Default, Clone, Eq, PartialEq)]
 pub struct EditorConfigProperties {
     /// Set to tab or space to use tabs or spaces for indentation, respectively.
     /// Option tab implies that an indentation is to be filled by as many hard tabs as possible, with the rest of the indentation filled by spaces.
     /// A non-normative explanation can be found in the indentation_ section.
     /// The values are case-insensitive.
-    pub indent_style: Option<IdentStyle>,
+    pub indent_style: EditorConfigProperty<IndentStyle>,
 
     /// Set to a whole number defining the number of columns used for each indentation level and the width of soft tabs (when supported).
     /// If this equals tab, the indent_size shall be set to the tab size, which should be tab_width (if specified); else, the tab size set by the editor.
     /// The values are case-insensitive.
-    pub indent_size: Option<usize>,
+    pub indent_size: EditorConfigProperty<usize>,
 
     /// Set to a whole number defining the number of columns used to represent a tab character.
     /// This defaults to the value of indent_size and should not usually need to be specified.
-    pub tab_width: Option<usize>,
+    pub tab_width: EditorConfigProperty<usize>,
 
     /// Set to lf, cr, or crlf to control how line breaks are represented.
     /// The values are case-insensitive.
-    pub end_of_line: Option<EndOfLine>,
+    pub end_of_line: EditorConfigProperty<EndOfLine>,
 
     /// Set to latin1, utf-8, utf-8-bom, utf-16be or utf-16le to control the character set.
     /// Use of utf-8-bom is discouraged.
     /// The values are case-insensitive.
-    pub charset: Option<Charset>,
+    pub charset: EditorConfigProperty<Charset>,
 
     /// Set to true to remove all whitespace characters preceding newline characters in the file and false to ensure it doesn’t.
-    pub trim_trailing_whitespace: Option<bool>,
+    pub trim_trailing_whitespace: EditorConfigProperty<bool>,
 
     /// Set to true ensure file ends with a newline when saving and false to ensure it doesn’t.
     /// Editors must not insert newlines in empty files when saving those files, even if insert_final_newline = true.
-    pub insert_final_newline: Option<bool>,
+    pub insert_final_newline: EditorConfigProperty<bool>,
 
     /// Prettier print width.
     /// Not part of spec <https://github.com/editorconfig/editorconfig-vscode/issues/53#issuecomment-462432616>
     /// But documented in <https://prettier.io/docs/next/configuration#editorconfig>
-    pub max_line_length: Option<MaxLineLength>,
+    pub max_line_length: EditorConfigProperty<MaxLineLength>,
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
@@ -74,32 +84,10 @@ pub enum MaxLineLength {
     Off,
 }
 
-impl MaxLineLength {
-    fn parse(s: &str) -> Option<Self> {
-        if s.eq_ignore_ascii_case("off") {
-            Some(Self::Off)
-        } else {
-            s.parse::<usize>().ok().map(Self::Number)
-        }
-    }
-}
-
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
-pub enum IdentStyle {
+pub enum IndentStyle {
     Tab,
     Space,
-}
-
-impl IdentStyle {
-    fn parse(s: &str) -> Option<Self> {
-        if s.eq_ignore_ascii_case("tab") {
-            Some(Self::Tab)
-        } else if s.eq_ignore_ascii_case("space") {
-            Some(Self::Space)
-        } else {
-            None
-        }
-    }
 }
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
@@ -109,20 +97,6 @@ pub enum EndOfLine {
     Crlf,
 }
 
-impl EndOfLine {
-    fn parse(s: &str) -> Option<Self> {
-        if s.eq_ignore_ascii_case("lf") {
-            Some(Self::Lf)
-        } else if s.eq_ignore_ascii_case("cr") {
-            Some(Self::Cr)
-        } else if s.eq_ignore_ascii_case("crlf") {
-            Some(Self::Crlf)
-        } else {
-            None
-        }
-    }
-}
-
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
 pub enum Charset {
     Latin1,
@@ -130,24 +104,6 @@ pub enum Charset {
     Utf8bom,
     Utf16be,
     Utf16le,
-}
-
-impl Charset {
-    fn parse(s: &str) -> Option<Self> {
-        if s.eq_ignore_ascii_case("utf-8") {
-            Some(Self::Utf8)
-        } else if s.eq_ignore_ascii_case("latin1") {
-            Some(Self::Latin1)
-        } else if s.eq_ignore_ascii_case("utf-16be") {
-            Some(Self::Utf16be)
-        } else if s.eq_ignore_ascii_case("utf-16le") {
-            Some(Self::Utf16le)
-        } else if s.eq_ignore_ascii_case("utf-8-bom") {
-            Some(Self::Utf8bom)
-        } else {
-            None
-        }
-    }
 }
 
 impl EditorConfig {
@@ -196,28 +152,31 @@ impl EditorConfig {
                 let properties = &mut section.properties;
                 match key.trim_end() {
                     "indent_style" => {
-                        properties.indent_style = IdentStyle::parse(value);
+                        properties.indent_style = IndentStyle::parse(value);
                     }
                     "indent_size" => {
-                        properties.indent_size = parse_number(value);
+                        properties.indent_size = EditorConfigProperty::<usize>::parse(value);
                     }
                     "tab_width" => {
-                        properties.indent_size = parse_number(value);
+                        properties.tab_width = EditorConfigProperty::<usize>::parse(value);
                     }
                     "end_of_line" => {
-                        properties.end_of_line = EndOfLine::parse(value);
+                        properties.end_of_line = EditorConfigProperty::<EndOfLine>::parse(value);
                     }
                     "charset" => {
-                        properties.charset = Charset::parse(value);
+                        properties.charset = EditorConfigProperty::<Charset>::parse(value);
                     }
                     "trim_trailing_whitespace" => {
-                        properties.trim_trailing_whitespace = parse_bool(value);
+                        properties.trim_trailing_whitespace =
+                            EditorConfigProperty::<bool>::parse(value);
                     }
                     "insert_final_newline" => {
-                        properties.insert_final_newline = parse_bool(value);
+                        properties.insert_final_newline =
+                            EditorConfigProperty::<bool>::parse(value);
                     }
                     "max_line_length" => {
-                        properties.max_line_length = MaxLineLength::parse(value);
+                        properties.max_line_length =
+                            EditorConfigProperty::<MaxLineLength>::parse(value);
                     }
                     _ => {}
                 }
@@ -226,25 +185,131 @@ impl EditorConfig {
 
         Self { root, sections }
     }
-}
 
-fn parse_number(s: &str) -> Option<usize> {
-    s.parse::<usize>().ok()
-}
-
-fn parse_bool(s: &str) -> Option<bool> {
-    if s.eq_ignore_ascii_case("true") {
-        Some(true)
-    } else if s.eq_ignore_ascii_case("false") {
-        Some(false)
-    } else {
-        None
+    /// Resolve a given path and return the resolved properties.
+    pub fn resolve(&self, path: &Path) -> EditorConfigProperties {
+        let mut properties = EditorConfigProperties::default();
+        for section in &self.sections {
+            let glob = Glob::new(&section.name).unwrap().compile_matcher();
+            if glob.is_match(path) {
+                properties.override_with(&section.properties);
+            }
+        }
+        properties
     }
 }
 
-impl EditorConfig {
-    /// Resolve a given path and return the resolved properties.
-    pub fn resolve(&self, path: &Path) -> EditorConfigProperties {
-        EditorConfigProperties::default()
+impl<T: Copy> EditorConfigProperty<T> {
+    fn override_with(&mut self, other: &Self) {
+        match other {
+            Self::Value(value) => {
+                *self = Self::Value(*value);
+            }
+            Self::Unset => {
+                *self = Self::None;
+            }
+            Self::None => {}
+        }
+    }
+}
+
+impl EditorConfigProperties {
+    fn override_with(&mut self, other: &Self) {
+        self.indent_style.override_with(&other.indent_style);
+        self.indent_size.override_with(&other.indent_size);
+        self.tab_width.override_with(&other.tab_width);
+        self.end_of_line.override_with(&other.end_of_line);
+        self.charset.override_with(&other.charset);
+        self.trim_trailing_whitespace.override_with(&other.trim_trailing_whitespace);
+        self.insert_final_newline.override_with(&other.insert_final_newline);
+        self.max_line_length.override_with(&other.max_line_length);
+    }
+}
+
+impl EditorConfigProperty<usize> {
+    fn parse(s: &str) -> Self {
+        if s.eq_ignore_ascii_case("unset") {
+            Self::Unset
+        } else {
+            s.parse::<usize>().map_or(Self::None, EditorConfigProperty::Value)
+        }
+    }
+}
+
+impl EditorConfigProperty<bool> {
+    fn parse(s: &str) -> Self {
+        if s.eq_ignore_ascii_case("true") {
+            EditorConfigProperty::Value(true)
+        } else if s.eq_ignore_ascii_case("false") {
+            EditorConfigProperty::Value(false)
+        } else if s.eq_ignore_ascii_case("unset") {
+            EditorConfigProperty::Unset
+        } else {
+            EditorConfigProperty::None
+        }
+    }
+}
+
+impl IndentStyle {
+    fn parse(s: &str) -> EditorConfigProperty<Self> {
+        if s.eq_ignore_ascii_case("tab") {
+            EditorConfigProperty::Value(Self::Tab)
+        } else if s.eq_ignore_ascii_case("space") {
+            EditorConfigProperty::Value(Self::Space)
+        } else if s.eq_ignore_ascii_case("unset") {
+            EditorConfigProperty::Unset
+        } else {
+            EditorConfigProperty::None
+        }
+    }
+}
+
+impl EditorConfigProperty<EndOfLine> {
+    fn parse(s: &str) -> Self {
+        if s.eq_ignore_ascii_case("lf") {
+            Self::Value(EndOfLine::Lf)
+        } else if s.eq_ignore_ascii_case("cr") {
+            Self::Value(EndOfLine::Cr)
+        } else if s.eq_ignore_ascii_case("crlf") {
+            Self::Value(EndOfLine::Crlf)
+        } else if s.eq_ignore_ascii_case("unset") {
+            Self::Unset
+        } else {
+            Self::None
+        }
+    }
+}
+
+impl EditorConfigProperty<Charset> {
+    fn parse(s: &str) -> Self {
+        if s.eq_ignore_ascii_case("utf-8") {
+            Self::Value(Charset::Utf8)
+        } else if s.eq_ignore_ascii_case("latin1") {
+            Self::Value(Charset::Latin1)
+        } else if s.eq_ignore_ascii_case("utf-16be") {
+            Self::Value(Charset::Utf16be)
+        } else if s.eq_ignore_ascii_case("utf-16le") {
+            Self::Value(Charset::Utf16le)
+        } else if s.eq_ignore_ascii_case("utf-8-bom") {
+            Self::Value(Charset::Utf8bom)
+        } else if s.eq_ignore_ascii_case("unset") {
+            Self::Unset
+        } else {
+            Self::None
+        }
+    }
+}
+
+impl EditorConfigProperty<MaxLineLength> {
+    fn parse(s: &str) -> Self {
+        if s.eq_ignore_ascii_case("off") {
+            Self::Value(MaxLineLength::Off)
+        } else if s.eq_ignore_ascii_case("unset") {
+            Self::Unset
+        } else if let Ok(n) = s.parse::<usize>() {
+            Self::Value(MaxLineLength::Number(n))
+        } else {
+            Self::None
+        }
     }
 }
